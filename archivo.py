fe3e173c9a00878d20a73c25e18d839364a1ca41
John Cerpa <jjcerpa@uninorte.edu.co>
2020-03-14 19:17:55 -0500

    dos versiones

--- archivo.py
+++ archivo.py
@@ -1,4 +1,19 @@
 def main():
-  i = 5L + 3
-  part =? True
-  return "Hola mundo"
+  ans = False
+  for i in range(len(exp)):
+    part =? True
+    for j in range(len(exp[i])):
+      part = part and xi[exp[i][j]]
+      ans = ans or part
+      return ans
+      while 2*i<=40000L
+      h = 3.4e-5 + 5j
+      if i == 0:
+        break
+        def otra_fun():
+          pass
+          print('Esta es la respuesta',con, 'y la expresión ', 3*i+5*(7-x))
+          #strings
+          table = []
+          y = []
+          #i = 0
